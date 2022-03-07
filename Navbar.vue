3f434997ab52f2ce132eee4c171535494b677171
zach <62062622+zach12904@users.noreply.github.com>
2022-03-07 08:53:02 -0500

    Add files via upload

---
+++ Navbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <nav>
+      <h1>My Book List</h1>
+
+      <!-- for logged in users -->
+      <div>
+        <router-link to="/">Home</router-link>
+        <button @click="handleClick">Logout</button>
+      </div>
+
+      <!-- for logged out users -->
+      <div>
+        <router-link to="/login">Login</router-link>
+        <router-link to="/signup">Signup</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import getUser from '../composables/getUser'
+
+// firebase imports
+import { auth } from '../firebase/config'
+import { signOut } from 'firebase/auth'
+
+export default {
+  setup() {
+    const { user } = getUser()
+
+    const handleClick = () => {
+      signOut(auth)
+    }
+
+    return { handleClick, user }
+  }
+}
+</script>
+
+<style>
+nav {
+  display: flex;
+  align-items: center;
+}
+nav h1 {
+  margin-right: auto;
+  margin-bottom: 0;
+}
+nav a {
+  margin-left: 16px;
+  color: #2c3e50;
+}
+nav button {
+  margin-left: 16px;
+}
+nav a.router-link-exact-active {
+  color: #0ec58e;
+}
+nav + p {
+  margin-top: 0;
+  margin-bottom: 30px;
+}
+</style>
